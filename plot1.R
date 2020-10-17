5caf5f3da1816e43346e569ddd81c052e1400f11
damelisa <kalidami@gmail.com>
2020-10-18 01:07:48 +0200

    Comment script

--- plot1.R
+++ plot1.R
@@ -1,9 +1,18 @@
+# Load libraries
 library(chron)
+
+# Read table and subset
 hpc <- read.table("./household_power_consumption.txt", header = TRUE, sep = ";")
 data <- subset(hpc, Date %in% c("1/2/2007","2/2/2007"))
+
+# Format time and date columns and convert values into numeric
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 data$Time <- chron(times. = data$Time)
 data[,3:9] <- sapply(data[,3:9],as.numeric)
+
+# Plot graph
 hist(data$Global_active_power, col="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+# Write plot into png graphic device
 dev.copy(png, filename = "plot1.png", width = 480, height = 480, units = "px")
 dev.off()
